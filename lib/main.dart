2fe2388b1abedc52e40c8cec113b1ce9748b9983
yuzo <spacemajestic@yahoo.co.jp>
2022-04-29 20:39:33 +0900

    water_drop_nav_bar

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'SymbolPage.dart';
 import 'NextPage.dart';
 
 import 'package:provider/provider.dart';
+import 'package:water_drop_nav_bar/water_drop_nav_bar.dart';
 
 final RouteObserver<ModalRoute> routeObserver = RouteObserver<ModalRoute>();
 void main() {
@@ -52,13 +53,7 @@ class _MyStatefulWidget extends State<MyStatefulWidget> {
   List<Widget> pageList = <Widget>[];
   int _selectedIndex = 1;
   final PageController _pageController = PageController(initialPage: 1);
-  void _onTappedBar(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-    _pageController.animateToPage(index,
-        duration: const Duration(milliseconds: 10), curve: Curves.ease);
-  }
+
   // void _onTappedBar(int value) { // pageview
   //   setState(() {
   //     _selectedIndex = value;
@@ -87,6 +82,8 @@ class _MyStatefulWidget extends State<MyStatefulWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey.shade300,
+
       body:
           // appBar: AppBar(
           //   title: const Text('Select symbol'),
@@ -107,24 +104,53 @@ class _MyStatefulWidget extends State<MyStatefulWidget> {
       //   // globalKey_GeneratorPageState.currentState?.createSymbolSet();
       // },
 
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: _onTappedBar,
-        currentIndex: _selectedIndex,
-        type: BottomNavigationBarType.fixed,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'SYMBOLS',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'HOME',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.volunteer_activism),
-            label: 'DONATION',
-          ),
-        ],
+      // bottomNavigationBar: BottomNavigationBar(
+      //   onTap: _onTappedBar,
+      //   currentIndex: _selectedIndex,
+      //   type: BottomNavigationBarType.fixed,
+      //   items: const [
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.settings),
+      //       label: 'SYMBOLS',
+      //     ),
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.home),
+      //       label: 'HOME',
+      //     ),
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.volunteer_activism),
+      //       label: 'DONATION',
+      //     ),
+      //   ],
+      // ),
+      bottomNavigationBar: ClipRRect(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+        child: WaterDropNavBar(
+          // backgroundColor:Colors.grey.shade300,
+          inactiveIconColor: Colors.grey.shade300,
+          waterDropColor: Colors.grey.shade300,
+          backgroundColor: Colors.green,
+          onItemSelected: (int index) {
+            setState(() {
+              _selectedIndex = index;
+            });
+            _pageController.animateToPage(_selectedIndex,
+                duration: const Duration(milliseconds: 400),
+                curve: Curves.easeOutQuad);
+          },
+          selectedIndex: _selectedIndex,
+          barItems: <BarItem>[
+            BarItem(
+              filledIcon: Icons.settings,
+              outlinedIcon: Icons.settings_outlined,
+            ),
+            BarItem(filledIcon: Icons.home, outlinedIcon: Icons.home_outlined),
+            BarItem(
+              filledIcon: Icons.volunteer_activism,
+              outlinedIcon: Icons.volunteer_activism_outlined,
+            ),
+          ],
+        ),
       ),
     );
   }
