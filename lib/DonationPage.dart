15f8294ccc05c3d2bf3c418aeb26b2d0d28c7d7a
yuzo <spacemajestic@yahoo.co.jp>
2022-05-23 19:44:16 +0900

    release3

--- lib/DonationPage.dart
+++ lib/DonationPage.dart
@@ -1,7 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'dart:async';
 
 import 'widgets/AdBanner.dart';
+
+class DonationPage3 extends StatefulWidget {
+  const DonationPage3({Key? key}) : super(key: key);
+
+  @override
+  State<DonationPage3> createState() => _DonationPage();
+}
+
+class _DonationPage extends State<DonationPage3> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  bool _isLoading = true;
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade300,
+      appBar: AppBar(
+        title: const Text('Please Click'),
+      ),
+      body: Center(
+          child: _isLoading
+              ? AdBanner(size: AdSize.banner)
+              : CircularProgressIndicator()),
+    );
+  }
+}
 
 class DonationPage extends StatelessWidget {
   const DonationPage({Key? key}) : super(key: key);
@@ -16,14 +46,14 @@ class DonationPage extends StatelessWidget {
         body: Center(
           child: Column(mainAxisSize: MainAxisSize.min, children: const [
             AdBanner(size: AdSize.banner),
-            SizedBox(
-              height: 30,
-            ),
-            AdBanner(size: AdSize.banner),
-            SizedBox(
-              height: 30,
-            ),
-            AdBanner(size: AdSize.banner),
+            // SizedBox(
+            //   height: 30,
+            // ),
+            // AdBanner(size: AdSize.banner),
+            // SizedBox(
+            //   height: 30,
+            // ),
+            // AdBanner(size: AdSize.banner),
           ]),
         ));
   }
--- lib/widgets/AdBanner.dart
+++ lib/widgets/AdBanner.dart
@@ -2,8 +2,64 @@ import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'dart:io';
 
-class AdBanner extends StatelessWidget {
-  const AdBanner({
+class AdBanner extends StatefulWidget {
+  const AdBanner({required this.size, Key? key}) : super(key: key);
+  final AdSize size;
+
+  @override
+  State<AdBanner> createState() => _AdBanner();
+}
+
+class _AdBanner extends State<AdBanner> {
+  bool _isAdLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    debugPrint('Ad initState.');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final banner = BannerAd(
+        size: AdSize.banner,
+        adUnitId: bannerAdUnitId,
+        listener: BannerAdListener(
+          onAdLoaded: (Ad ad) => setState(() {
+            debugPrint('Ad loaded.');
+            _isAdLoading = false;
+          }),
+          onAdFailedToLoad: (Ad ad, LoadAdError error) {
+            debugPrint('Ad failed to load: $error');
+          },
+          onAdOpened: (Ad ad) => debugPrint('Ad opened.'),
+          onAdClosed: (Ad ad) => debugPrint('Ad closed.'),
+        ),
+        request: const AdRequest())
+      ..load();
+
+    return SizedBox(
+        width: banner.size.width.toDouble(),
+        height: banner.size.height.toDouble(),
+        child:
+            _isAdLoading ? CircularProgressIndicator() : AdWidget(ad: banner));
+  }
+
+  // 広告ID
+  static String get bannerAdUnitId {
+    if (Platform.isAndroid) {
+      return 'ca-app-pub-6147471144580591/7187192436';
+    } else if (Platform.isIOS) {
+      return 'ca-app-pub-6147471144580591/9464450539';
+    } else {
+      //どちらでもない場合は、テスト用を返す
+      return 'ca-app-pub-3940256099942544/2934735716';
+    }
+  }
+}
+
+class AdBanner2 extends StatelessWidget {
+  const AdBanner2({
     required this.size,
     Key? key,
   }) : super(key: key);
